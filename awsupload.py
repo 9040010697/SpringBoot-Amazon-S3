becb6d0bc3e576c707204db0526141666a4edc94
Dhananjaya Samanta Singhar <52064780+dhananjaysinghar@users.noreply.github.com>
2024-08-07 21:49:16 +0530

    Update awsupload.py

--- awsupload.py
+++ awsupload.py
@@ -5,6 +5,61 @@ import gc
 import math
 from datetime import datetime
 
+# Default minimum size of file should be 5MB
+def upload_parquet_to_s3_with_transaction(dataframe, bucket_name, s3_file_key, aws_access_key_id, aws_secret_access_key, min_chunk_size=5 * 1024 * 1024):
+    # Create a boto3 client
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    
+    # Convert the entire DataFrame to a Parquet file in memory
+    parquet_buffer = BytesIO()
+    dataframe.to_parquet(parquet_buffer, index=False, compression='gzip')
+    parquet_buffer.seek(0)
+    
+    # Check the size of the buffer
+    buffer_size = parquet_buffer.getbuffer().nbytes
+
+    if buffer_size <= min_chunk_size:
+        # Single upload if the buffer size is smaller than or equal to the minimum chunk size
+        s3_client.put_object(Bucket=bucket_name, Key=s3_file_key, Body=parquet_buffer)
+        print(f"File uploaded to s3://{bucket_name}/{s3_file_key} using single upload")
+    else:
+        # Multipart upload if the buffer size is larger than the minimum chunk size
+        multipart_upload = s3_client.create_multipart_upload(Bucket=bucket_name, Key=s3_file_key)
+        upload_id = multipart_upload['UploadId']
+        parts = []
+
+        try:
+            part_number = 1
+            while parquet_buffer.tell() < buffer_size:
+                chunk_data = parquet_buffer.read(min_chunk_size)
+                if len(chunk_data) == 0:
+                    break
+                part = s3_client.upload_part(
+                    Bucket=bucket_name,
+                    Key=s3_file_key,
+                    PartNumber=part_number,
+                    UploadId=upload_id,
+                    Body=chunk_data
+                )
+                parts.append({'PartNumber': part_number, 'ETag': part['ETag']})
+                part_number += 1
+
+            # Complete multipart upload
+            s3_client.complete_multipart_upload(
+                Bucket=bucket_name,
+                Key=s3_file_key,
+                UploadId=upload_id,
+                MultipartUpload={'Parts': parts}
+            )
+            print(f"File uploaded to s3://{bucket_name}/{s3_file_key} using multipart upload")
+        except Exception as e:
+            s3_client.abort_multipart_upload(Bucket=bucket_name, Key=s3_file_key, UploadId=upload_id)
+            print(f"Upload failed and aborted: {e}")
+
 def upload_dataframe_in_chunks(dataframe, bucket_name, s3_file_key_prefix, aws_access_key_id, aws_secret_access_key, chunk_size=10):
     # Create a boto3 client
     s3_client = boto3.client(
